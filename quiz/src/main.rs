8226e2c0b3170a8822d87c53cd3483b64e8b3bce
Degreat <mail@degreat.co.uk>
2024-01-16 20:27:51 +0000

    gui

--- quiz/src/main.rs
+++ quiz/src/main.rs
@@ -1,5 +1,33 @@
 mod quiz;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native("Queeez", options, Box::new(|ctx| Box::new(App::new())))
+}
+
+struct App {}
+
+impl App {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("1/10");
+            ui.label("What is the capital of Nigeria?");
+
+            ui.horizontal(|ui| {
+                ui.button("True");
+                ui.button("False");
+            });
+
+            ui.horizontal(|ui| {
+                ui.button("Previous");
+                ui.button("Next");
+            });
+        });
+    }
 }
